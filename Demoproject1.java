4aed9e7696d06fc6da3098bf51758bf9d7a3caf3
sirishasiri1 <psireeshareddy35@gmail.com>
2018-10-08 23:56:56 +0530

    first commit

---
+++ Demoproject1.java
@@ -0,0 +1,17 @@
+public class Demoproject1{
+             public static void main(String[] args){
+System.out.println("this is my Demoproject1");
+System.out.println("this is my secondProject1");
+System.out.println("this is my thirdProject1");
+System.out.println("this is my fourthProject1");
+System.out.println("this is my fifthProject1");
+   }
+}
+
+
+
+
+  
+               
+
+                 
